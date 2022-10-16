df564ca4e2e888708e508b5ef616a5b9d9ea7fd0
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-10-16 14:58:34 -0600

    added

--- geometry.erl
+++ geometry.erl
@@ -2,4 +2,5 @@
 -export([area/1]).
 
 area({rectangle, Width, Height}) -> Width * Height;
+area({square, Side}) -> Side * Side;
 area({circle, R}) -> 3.1415926 * R * R.
---
+++ shop.erl
@@ -0,0 +1,8 @@
+-module(shop).
+-export([cost/1]).
+
+cost(orange) -> 5;
+cost(newspaper) -> 6;
+cost(apples) -> 2;
+cost(pear) -> 9;
+cost(milk) -> 7.

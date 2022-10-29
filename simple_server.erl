7484a7feebae38b054755b9128c7ac0dc78438a0
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-10-28 23:45:50 -0600

    added

---
+++ simple_server.erl
@@ -0,0 +1,41 @@
+% Simple_Server_Version_1.1
+
+-module(simple_server).
+-export([start/0, message/2]).
+
+start() -> spawn(fun loop/0).
+
+message(Pid, What) ->
+    rpc(Pid, What).
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
+
+loop() ->
+    receive
+        {From, {yong, Message}} ->
+            From ! {self(), Message},
+            loop
+    end.
+
+% simpleServer -- version_1.0
+% -module(test_server).
+% -export([loop/0, rpc/2]).
+
+% rpc(Pid, Request) ->
+%     Pid ! {self(), Request},
+%     receive
+%         {Pid, Response} ->
+%             Response
+%     end.
+
+% loop() ->
+%     receive
+%         {From, {yong, Message}} ->
+%             From ! {self(), Message},
+%             loop()
+%     end.

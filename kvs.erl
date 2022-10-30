ec21097043fc141711897c02ec4d872646251e96
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-10-30 16:36:37 -0600

    kvs server added

---
+++ kvs.erl
@@ -0,0 +1,26 @@
+-module(kvs).
+-export([start/0, store/2, loopup/1]).
+
+start() -> register(kvs, spawn(fun() -> loop() end)).
+
+store(Key, Value) -> rpc({store, Key, Value}).
+
+loopup(Key) -> rpc({loopup, Key}).
+
+rpc(Q) ->
+    kvs ! {self(), Q},
+    receive
+        {kvs, Reply} ->
+            Reply
+    end.
+
+loop() ->
+    receive
+        {From, {store, Key, Value}} ->
+            put(Key, {ok, Value}),
+            From ! {kvs, true},
+            loop();
+        {From, {loopup, Key}} ->
+            From ! {kvs, get(Key)},
+            loop()
+    end.
